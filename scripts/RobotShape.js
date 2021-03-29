4c5a6c5fe921d392380cbaa3322c54a3e6a0420b
Tim Collins <47898052+drtimcollins@users.noreply.github.com>
2021-03-29 14:45:30 +0100

    fix checksize bug

--- scripts/RobotShape.js
+++ scripts/RobotShape.js
@@ -82,7 +82,6 @@ class RobotShape extends THREE.Group{
             var sensorBoxMesh = new THREE.Mesh(sensorBoxg, sensorBoxMat);
             for(var n = 0; n < MAXSENSORS; n++){
                 var sensorN = sensorLEDMesh.clone();
-                //sensorN.rotateX(-Math.PI/2);
                 sensorN.position.set(this.robotLength, (n - (this.NumberOfSensors-1.0)/2.0)*this.SensorSpacing, -11.5);
                 this.sensors.push(sensorN);
                 var sensorBoxN = sensorBoxMesh.clone();
@@ -185,13 +184,16 @@ class RobotShape extends THREE.Group{
     }
 
     checkSize(){
-        let p0 = new THREE.Vector2(7 + this.robotLength, 7 + this.SensorSpacing*(this.NumberOfSensors-1)/2);
-        let p1 = new THREE.Vector2(-20, this.robotWidth/2 + 4);
+        let p0 = new THREE.Vector2(7 + this.robotLength, 7 + this.SensorSpacing*(this.NumberOfSensors-1)/2); // Edge of sensors
+        let p1 = new THREE.Vector2(-20, this.robotWidth/2 + 4);                                              // Back of wheel
+        let p2 = new THREE.Vector2(20, this.robotWidth/2 + 4);                                               // Front of wheel
         this.xOffset = (p1.lengthSq() - p0.lengthSq()) / 2 / (p1.x - p0.x);
         let org = new THREE.Vector2(this.xOffset, 0);
         this.radius = p0.distanceTo(org);
-        //console.log(p0.distanceTo(org).toString() + " --- " + p1.distanceTo(org).toString());
-        //console.log(p0.clone().sub(org).length().toString() + " --- " + p1.clone().sub(org).length().toString());
+        if(p2.distanceTo(org) > this.radius){
+            this.xOffset = 0;
+            this.radius = p1.length();
+        }
     }
 }
 
